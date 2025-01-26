4bcfe0d6143ced054ad9e6903c7e2c575a7e71f9
auroraVasconcelos <aurorabsvasconcelos@gmail.com>
2025-01-26 17:42:26 +0000

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
             fontFamily: {
                 poppins: ["Poppins", "sans-serif"],
                 manrope: ["Manrope", "sans-serif"],
+                onabort: ["Onabort", "sans-serif"],
             },
         },
     },
